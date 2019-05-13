1687697e683cfd8980d90543bca4ed6d283b03e3
10710280@cityplym.ac.uk <10710280@cityplym.ac.uk>
2019-05-13 23:04:44 +0100

    Created array

--- src/com/company/Location.java
+++ src/com/company/Location.java
@@ -3,5 +3,12 @@ package com.company;
 public class Location {
 
 
+    public static void gameWorld() {
+        String[][] map = new String[6][6];
+        System.out.println(map.length);
 
+
+
+
+    }
 }
