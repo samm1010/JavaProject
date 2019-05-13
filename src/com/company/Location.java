27e31a6d4d64148ee022170d90a4a02cefb7d790
10710280@cityplym.ac.uk <10710280@cityplym.ac.uk>
2019-05-13 23:29:29 +0100

    ADDED LOCATIONS

--- src/com/company/Location.java
+++ src/com/company/Location.java
@@ -4,8 +4,33 @@ public class Location {
 
 
     public static void gameWorld() {
-        String[][] map = new String[6][6];
-        System.out.println(map.length);
+        String[][] map = new String[5][5];
+        map[0][0] = "You have arrived at ";
+        map[0][1] = "You have arrived at the mall OBJECT 1 HERE";
+        map[0][2] = "You have arrived at ";
+        map[0][3] = "You have arrived at ";
+        map[0][4] = "You have arrived at ";
+        map[1][0] = "You have arrived at ";
+        map[1][1] = "You have arrived at ";
+        map[1][2] = "You have arrived at ";
+        map[1][3] = "You have arrived at ";
+        map[1][4] = "You have arrived at ";
+        map[2][0] = "You have arrived at ";
+        map[2][1] = "You have arrived at ";
+        map[2][2] = "You have arrived at ";
+        map[2][3] = "You have arrived at ";
+        map[2][4] = "You have arrived at ";
+        map[3][0] = "You have arrived at ";
+        map[3][1] = "You have arrived at ";
+        map[3][2] = "You have arrived at ";
+        map[3][3] = "You have arrived at ";
+        map[3][4] = "You have arrived at ";
+        map[4][0] = "You have arrived at ";
+        map[4][1] = "You have arrived at the hospital OBJECT 2 HERE";
+        map[4][2] = "You have arrived at ";
+        map[4][3] = "You have arrived at ";
+        map[4][4] = "You have arrived at the police station OBJECT 3 HERE";
+        System.out.println(map[0][1]);
 
 
 
